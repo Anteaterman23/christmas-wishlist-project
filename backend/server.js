ac9b2551160423699ca5f8943b98616f43740693
Tyler Bennett <tjosephb2003@gmail.com>
2026-06-21 13:24:46 -0400

    fix: Backend CORS config

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,22 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.LOCALHOST_URL,
+];
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+}));
 app.use(express.json());
 
 // MongoDB Connection
